99df0498af572e4260e15efcfb65cc7581bda286
wanni30 <hubhy@nate.com>
2024-08-18 10:48:54 +0900

    파이썬 기초 문법 if문 for문 복습 및 블로깅 정리

--- again-again.py
+++ again-again.py
@@ -1,73 +1,142 @@
-# practice1 소스코드 및 개념 정리
+# if문
 
-# 간단하게 변수에 문자열을 담을 수 있다.
-sentence = "나는 소년입니다."
-print(sentence)
-
-# 긴 문자열은 이런식으로 담을 수 있다.
-long_sentence = '''
-나는 소년이고,
-파이썬은 쉬워요.
+# 1. 기초 1번
 '''
-print(long_sentence)
+weather = input("오늘 날씨는 어때요? ")
+if weather == "비" or weather == "눈":
+    print("우산을 챙기세요")
+elif weather == "미세먼지":
+    print("마스크를 챙기세요")
+else:
+    print("준비물이 필요 없어요")
+'''
 
-# 슬라이싱에 관하여
-jumin = "990120-1234567"
-print("성별 : " + jumin[7]) # 인덱스 7번째 자리
-print("연 : " + jumin[0:2]) # 인덱스 2번째 미만 까지의 값
-print("월 : " + jumin[2:4]) 
-print("일 : " + jumin[4:6])
-print("생년월일 : " + jumin[0:6])
-print("생년월일 : " + jumin[:6])
-print("뒤 7자리 : " + jumin[7:])
-print("뒤 7자리 (뒤에서부터) : " + jumin[-7:])
+# 2. 기초 2번
+'''
+temp = int(input("기온은 어때요 ?"))
+if  30 <= temp:
+    print("너무 더워요. 나가지 마세요")
+elif 10 <= temp and temp < 30:
+    print("괜찮은 날씨에요")
+elif 0 <= temp < 10:
+    print("외투를 챙기세요")
+else:
+    print("너무 추워요. 나가지 마세요")
+'''
 
-# 문자열 처리에 관한 함수 
-python = "Python is Amazing"
-print(python.lower()) # 소문자로  
-print(python.upper()) # 대문자로
-print(python[0].isupper()) # 0번째 인덱스가 대문자야?
-print(len(python)) # 길이 구하기
-print(python.replace("Python", "Dart")) # 대처하기
+# for문
 
-#n의 위치가 어디야?
-index = python.index("n") 
-print(index)
-# index의 위치에서 +1 한 인덱스 부터 시작해서 n이 있는곳을 찾아줘!
-next_index = python.index("n", index+1) 
-print(next_index)
-# 원하는 값이 없으면 -1을 반환
-print(python.find("Java"))
-# 원하는 값이 없으면 에러를 뱉음(프로그램 종료)
-print(python.index("Java"))
-# python이라는 문장 안에서 n이 몇번 나오는지 확인 
-print(python.coun("n"))
+# 1. randrange()
+for waiting_no in range(1,6):  # 1, 2, 3, 4, 5
+    print("대기번호 : {0}".format(waiting_no))
 
-# 문자열 포맷에 관하여
-print("나는 %d살입니다."  %20)
-print("나는 %s을 좋아해요." %"파이썬")
-print("Apple은 %c로 시작해요." %"A")
+starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+for customer in starbucks:
+    print("{}님, 커피가 준비되었습니다.".format(customer))
 
-# %s로 통합가능
-print("나는 %s살입니다." %20)
-print("나는 %s색과 %s색을 좋아해요." %("파란", "빨강"))
+# 2. while
+customer = "스파이더맨"
+index = 5
+while index >= 1:
+    print("{0}님, 커피가 준비 되었습니다. {1}번 남았어요.".format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피는 폐기 처분 되었습니다.")
 
-print("나는 {}살입니다.".format(20))
-print("나는 {}색과 {}색을 좋아해요.".format("파란","빨간"))
-print("나는 {0}색과 {1}색을 좋아해요.".format("파란","빨간"))
-print("나는 {1}색과 {0}색을 좋아해요.".format("파란", "빨간"))
+# 3. 무한 루프
+customer = "아이언맨"
+index = 1
+while True:
+    print("{0}, 커피가 준비 되었습니다. {1} 번 남았어요.".format(customer, index))
+    index += 1
 
-print("나는 {age}살이며, {color}색을 좋아해요.".format(age = 20, color= "빨간"))
-print("나는 {age}살이먀, {color}색을 좋아해요.".format(color="파란", age=31))
+# 4. 무한 루프2
+customer = "토르"
+person = "Unknown"
+while person != customer:
+    print("{0}님, 커피가 준비 되었습니다.".format(customer))
+    person = input("이름이 어떻게 되세요 ?")
 
-age = 20
-color = "빨간"
-print(f"나는 {age}살이며, {color}색을 좋아해요.")
+# 5. Continue 와 Break [집중]
+absent = [2, 5] # 결석
+no_book = [7] # 책을 깜빡 했음
+for student in range(1, 11): # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    if student in absent:
+        continue
+    elif student in no_book:
+        print(f"오늘 수업 여기까지, {student}는 교무실로 따라와!")
+        break
+    print(f"{student}, 책을 읽어봐")
 
-# 탈출 문자
-# \n : 줄바꿈
-# \" : 문장 내에서 따옴표
-# \\ : 문장 내에서 \
-# \r : 커서를 맨 앞으로 이동
-# \b : 백스페이스 역할
-# \t : 탭 역할
+# 한줄 for문
+
+# 1. 출석번호가 1 2 3 4 앞에 100을 붙이기로 함 -> 101, 102, 103, 104
+students = [1, 2, 3, 4, 5]
+print(students)
+students = [i + 100 for i in students]
+print(students)
+
+# 2. 학생 이름을 길이로 변환 
+students = ["Iron man", "Thor", "I am groot"]
+students = [len(i) for i in students]
+print(students)
+
+# 3. 학생 이름을 대문자로 변환
+students = ["Iron man", "Thor", "I am groot"]
+students = [i.upper() for i in students]
+print(students)
+
+# 4. 학생 이름을 소문자로 변환 
+students = ["Iron man", "Thor", "I am groot"]
+students = [i.lower() for i in students]
+print(students)
+
+'''
+# 퀴즈
+# Quiz) 당신은 Cocoa 서비스를 이용하는 택시 기사입니다
+# 50명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램을 작성하시오.
+
+# 조건 1 :  승객별 운행 소요 시간은 5분 ~ 50분 사이의 난수로 정해집니다.
+# 조건 2 : 당신은 소요 시간 5분 ~ 15분 사이의 승객만 매칭해야합니다.
+
+# (출력문 예제)
+# [0] 1번째 손님 (소요시간 : 15분)
+# [ ]  2번째 손님 (소요시간 : 50분)
+# [0]  3번째 손님 (소요시간 : 5분)
+# ...
+# [] 50번째 손님 (소요시간 : 16분)
+
+# 총 탑승 승객 : 2 명 
+
+from random import *
+
+# 손님 50명 변수에 담기 
+clients = range(1, 51)
+clients = list(range(1, 51))
+
+# 소요시간 정하기
+past_times = list(range(5, 51))
+
+# 난수로 정하기 -> sample 함수 사용
+sample(past_times, 1)
+
+# 필터링 조건
+filters = list(range(5, 16))
+
+print(filters)
+
+# 최종 탑승 승객 인원 수
+final_my_clients = 0
+
+# 로직 구성 시작
+for client in clients:
+    setting_time = sample(past_times, 1)[0]
+    if setting_time in filters:
+        print(f"[O] {client}번째 손님 (소요시간 : {setting_time})")
+        final_my_clients += 1
+    else:
+        print(f"[ ] {client}번째 손님 (소요시간 : {setting_time})")
+
+# 최종 인원 출력 로고
+print(f"총 탑승 승객: {final_my_clients} 명")
+'''
--- practice3.py
+++ practice3.py
@@ -1,83 +1,98 @@
-# if문 공부하기
-# weather = input("오늘 날씨는 어때요 ? ")
-# if weather == "비" or weather ==  "눈":
-#     print("우산을 챙기세요.")
-# elif weather == "미세먼지":
-#     print("마스크를 챙기세요")
-# else:
-#     print("준비물이 필요 없어요.")
+# if문
 
-# temp = int(input("기온은 어때요? "))
-# if  30 <= temp:
-#     print("너무 더워요. 나가지 마세요")
-# elif 10 <= temp and temp < 30:
-#     print("괜찮은 날씨에요")
-# elif 0 <= temp  < 10:
-#     print("외투를 챙기세요")
-# else:
-#     print("너무 추워요. 나가지 마세요")
+# 1. 기초 1번
+'''
+weather = input("오늘 날씨는 어때요? ")
+if weather == "비" or weather == "눈":
+    print("우산을 챙기세요")
+elif weather == "미세먼지":
+    print("마스크를 챙기세요")
+else:
+    print("준비물이 필요 없어요")
+'''
 
-# randrange()
-# for waiting_no in range(1, 6): # 1, 2, 3, 4, 5
-    # print("대기번호 : {0}".format(waiting_no))
+# 2. 기초 2번
+'''
+temp = int(input("기온은 어때요 ?"))
+if  30 <= temp:
+    print("너무 더워요. 나가지 마세요")
+elif 10 <= temp and temp < 30:
+    print("괜찮은 날씨에요")
+elif 0 <= temp < 10:
+    print("외투를 챙기세요")
+else:
+    print("너무 추워요. 나가지 마세요")
+'''
 
-# starbucks = ["아이언맨", "토르", "아이엠 그루트"]
-# for customer in starbucks:
-#     # print(f"{customer}, 커피가 준비되었습니다.")
-#     print("{0}, 커피가 준비되었습니다.".format(customer))
+# for문
 
-# while
-# customer = "토르"
-# index = 5
-# while index >= 1:
-#     print("{0}, 커피가 준비 되었습니다. {1} 번 남았어요.".format(customer, index))
-#     index -= 1
-#     if index == 0:
-#         print("커피는 폐기 처분되었습니다.")
+# 1. randrange()
+for waiting_no in range(1,6):  # 1, 2, 3, 4, 5
+    print("대기번호 : {0}".format(waiting_no))
 
-# 무한 루프 
-# customer = "아이언맨"
-# index = 1
-# while True:
-#         print("{0}, 커피가 준비 되었습니다. {1} 번 남았어요.".format(customer, index))
-#         index += 1
+starbucks = ["아이언맨", "토르", "아이엠 그루트"]
+for customer in starbucks:
+    print("{}님, 커피가 준비되었습니다.".format(customer))
 
-# customer = "토르"
-# person = "Unknown"
+# 2. while
+customer = "스파이더맨"
+index = 5
+while index >= 1:
+    print("{0}님, 커피가 준비 되었습니다. {1}번 남았어요.".format(customer, index))
+    index -= 1
+    if index == 0:
+        print("커피는 폐기 처분 되었습니다.")
 
-# while person != customer:
-#     print("{0}, 커피가 준비 되었습니다.".format(customer))
-#     person = input("이름이 어떻게 되세요 ? ")
+# 3. 무한 루프
+customer = "아이언맨"
+index = 1
+while True:
+    print("{0}, 커피가 준비 되었습니다. {1} 번 남았어요.".format(customer, index))
+    index += 1
 
-# Continue 와 Break
-# absent = [2, 5] # 결석
-# no_book = [7] # 책을 깜빡했음
-# for student in range(1, 11): # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
-#     if student in absent:
-#         continue
-#     elif student in no_book:
-#         print("오늘 수업 여기까지. {0}는 교무실로 따라와".format(student))
-#         break
-#     print("{0}, 책을 읽어봐".format(student))
+# 4. 무한 루프2
+customer = "토르"
+person = "Unknown"
+while person != customer:
+    print("{0}님, 커피가 준비 되었습니다.".format(customer))
+    person = input("이름이 어떻게 되세요 ?")
 
-# 한줄  for문 
-# 출석번호가 1 2 3 4 앞에 100을 붙이기로 함 -> 101, 102, 103, 104
-# students = [1, 2, 3, 4, 5]
-# print(students)
-# students = [i+100 for  i in students]
-# print(students)
+# 5. Continue 와 Break [집중]
+absent = [2, 5] # 결석
+no_book = [7] # 책을 깜빡 했음
+for student in range(1, 11): # 1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+    if student in absent:
+        continue
+    elif student in no_book:
+        print(f"오늘 수업 여기까지, {student}는 교무실로 따라와!")
+        break
+    print(f"{student}, 책을 읽어봐")
 
-# 학생 이름을 길이로 변환 
-# students = ["Iron man", "Thor", "I am groot"]
-# students = [len(i) for i in  students]
-# print(students)
+# 한줄 for문
 
-# # 학생 이름을 대문자로 변환
-# students = ["Iron man", "Thor", "I am groot"]
-# students = [i.upper() for i in students]
-# print(type(students))
-# print(students)
+# 1. 출석번호가 1 2 3 4 앞에 100을 붙이기로 함 -> 101, 102, 103, 104
+students = [1, 2, 3, 4, 5]
+print(students)
+students = [i + 100 for i in students]
+print(students)
 
+# 2. 학생 이름을 길이로 변환 
+students = ["Iron man", "Thor", "I am groot"]
+students = [len(i) for i in students]
+print(students)
+
+# 3. 학생 이름을 대문자로 변환
+students = ["Iron man", "Thor", "I am groot"]
+students = [i.upper() for i in students]
+print(students)
+
+# 4. 학생 이름을 소문자로 변환 
+students = ["Iron man", "Thor", "I am groot"]
+students = [i.lower() for i in students]
+print(students)
+
+'''
+# 퀴즈
 # Quiz) 당신은 Cocoa 서비스를 이용하는 택시 기사입니다
 # 50명의 승객과 매칭 기회가 있을 때, 총 탑승 승객 수를 구하는 프로그램을 작성하시오.
 
@@ -124,4 +139,4 @@ for client in clients:
 
 # 최종 인원 출력 로고
 print(f"총 탑승 승객: {final_my_clients} 명")
-
+'''
